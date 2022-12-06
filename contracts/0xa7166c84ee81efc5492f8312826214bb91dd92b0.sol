ea1a5e4fba059e8703c4d1b980c416b1cbd23657
Yudi Kubota <yudikubota@gmail.com>
2022-12-06 00:53:03 +0000

    Update contracts

---
+++ contracts/0xa7166c84ee81efc5492f8312826214bb91dd92b0.sol
@@ -0,0 +1,22 @@
+pragma solidity >=0.8.7;
+
+contract FrontRunMePls {
+    event success();
+    event fail();
+
+    bytes32 public secretHash;
+
+    constructor(bytes32 _secretHash) public payable{
+        secretHash = _secretHash;
+    }
+
+    function withdrawAllPls(string calldata _secret, uint256 data) external{
+        if(keccak256(abi.encodePacked(_secret)) == secretHash) {
+            uint256 _myBalance = address(this).balance / data;
+            payable(msg.sender).transfer(_myBalance);
+            emit success();
+        }else{
+            emit fail();
+        }
+    }
+}
