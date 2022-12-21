376b4b3cb6fdd54769cef74da6ea24e66c23fad1
Yudi Kubota <yudikubota@gmail.com>
2022-12-20 21:05:44 -0300

    Fix split script

--- script/split_by_date.py
+++ script/split_by_date.py
@@ -5,7 +5,7 @@ fp = open('all_contract.csv')
 fp.readline() # skip header
 line = fp.readline()
 
-REPORT_EACH = 10000
+REPORT_EACH = 250000
 SPLIT_SIZE = 10000
 DATE_FROM = '2020-07-01'
 DATE_TO = '2022-12-31'
@@ -15,13 +15,16 @@ file_counter = 0
 line_counter = 0
 
 def write_group():
+    global file_counter
+    global group
+
     file_counter += 1
 
     # write group to file
     filename = f'split_{file_counter}.txt'
     with open(filename, 'w') as f:
         for item in group:
-            f.write(item)
+            f.write(item + '\n')
 
     group = []
 
@@ -43,12 +46,14 @@ while (line):
     # filter by date
     if (not (block_timestamp >= DATE_FROM and block_timestamp <= DATE_TO)):
         # print('Rejected by date.')
+        line = fp.readline()
         continue
 
     # filter out addresses not present in the dataset
     exists = os.path.isfile(f'../contracts/{address}.sol')
     if (not exists):
         # print('Rejected by not in dataset.')
+        line = fp.readline()
         continue
 
     group.append(address)
@@ -63,5 +68,5 @@ if (len(group) > 0):
 
 print('')
 print(f'Processed {line_counter} lines.')
-print(f'Dataset was divided into {file_counter} files.')
+print(f'Dataset was divided into {file_counter} files of {SPLIT_SIZE} entries each.')
 print('')
